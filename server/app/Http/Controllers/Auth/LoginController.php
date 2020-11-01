1b7001f42869c35caeaef4f9eb6869fa5276a115
shu-take <shingen9@gmail.com>
2020-11-01 16:50:12 +0900

    test

--- server/app/Http/Controllers/Auth/LoginController.php
+++ server/app/Http/Controllers/Auth/LoginController.php
@@ -37,5 +37,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
+    }gi
 }
--- server/routes/web.php
+++ server/routes/web.php
@@ -21,5 +21,6 @@ Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/library', 'LibraryController@index')->name('index');
+Route::get('/library', 'LibraryController@index')->name('index');
 
 Route::resource('library', 'LibraryController');
